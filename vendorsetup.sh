eb6505103538621abc7e49a6d016862478dbd393
Coulibaly Nonta <coulibalynonta@gmail.com>
2016-08-03 14:00:42 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 # Use this to add d5110_infinix to CM's lunch command menu
-add_lunch_combo cm_d5110_infinix-userdebug
-add_lunch_combo cm_d5110_infinix-eng
+for var in eng user userdebug; do
+  add_lunch_combo cm_d5110-$var
+done
